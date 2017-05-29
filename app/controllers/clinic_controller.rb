7f652ac822663c9bb86aa7e1b59c9da661190fbd
Wilfredo Porta <wporta@gmail.com>
2017-05-29 15:08:37 -0600

    Agregando funcionalidad en la pantalla de configuración de la Clínica.

--- app/controllers/clinic_controller.rb
+++ app/controllers/clinic_controller.rb
@@ -1,4 +1,26 @@
 class ClinicController < ApplicationController
-  def index
+
+  [:index, :show, :new].each do |method|
+    define_method(method) {redirect_to edit_clinic_path(2)}
   end
+
+  def edit
+    id = params[:id] || 2
+    @clinic = Clinic.find(id)
+  end
+
+  def update
+    id = params[:id] || 2
+    @clinic = Clinic.find(id)
+    begin
+      @clinic.attributes = clinic_params
+      @clinic.save!
+    end
+  end
+
+  def clinic_params
+    params.require(:clinic).permit(:name, :address)
+  end
+
+
 end
--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,18 +1,11 @@
 class Clinic < ActiveRecord::Base
-
-  self.primary_key = :id
-
-  attr_accessible :id, :name, :address, :phone, :email, :logo_url
-
   validates :name, presence: true
   validates :address, presence: true
-
 
   belongs_to :state
 
   def display_name
     self.name
   end
-
 
 end
