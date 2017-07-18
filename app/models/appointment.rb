eb4e0de22d02202b99a8408df83491c9ff2ba9e9
Wilfredo Porta <wporta@gmail.com>
2017-07-18 14:01:46 -0600

    Crear la funcionalidad de Búsqueda de Citas
    Se creó la funcionalidad para buscar las citas por nombre y apellido del paciente y por las notas.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,8 @@ class Appointment < ActiveRecord::Base
   validates :pacient_id, presence: true
   validates :appointment_date, presence: true
 
+  belongs_to :pacient
+
   def display_name
     self.appointment_date
   end
