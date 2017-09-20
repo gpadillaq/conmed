a0cd3a9df8cc3444aa2dd1c6c0b8f3ff99055f47
Wilfredo Porta <wporta@gmail.com>
2017-09-20 10:14:42 -0600

    Agregando las pantallas de Edición de la Consulta.

--- app/controllers/medical_consultations_controller.rb
+++ app/controllers/medical_consultations_controller.rb
@@ -4,6 +4,6 @@ class MedicalConsultationsController < ApplicationController
   private
 
   def medical_consultation_params
-    params.require(:medical_consultation).permit(:pacient_id)
+    params.require(:medical_consultation).permit(:pacient_id, :weight, :height, :blood_preasure, :symptoms, :diagnostic, :treatment, :test_results, :medical_consultation_date, :next_date, :cost)
   end
 end
